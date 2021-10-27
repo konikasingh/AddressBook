05f5fc3cade3545b0ba0e560082924955dd3316c
konikasingh <konikasingh1996@gmail.com>
2021-10-27 13:50:51 +0530

    User Contact List

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -7,6 +7,11 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book");
+
+            UC1AddressBook book = new UC1AddressBook(); // creating object of class
+
+            book.GetContactDetails();   //through object accessing method 
+            book.putDetails();
         }
     }
 }
