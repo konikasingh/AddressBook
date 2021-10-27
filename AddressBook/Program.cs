3c61d3d9ea39da64ae826fbac58867139c925d81
konikasingh <konikasingh1996@gmail.com>
2021-10-27 15:58:47 +0530

    Adding Data to Contact List

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -6,12 +6,53 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book");
+            UC2AddressBook book = new UC2AddressBook(); // creating object of class
+            string yes = "y";
+            string y;
 
-            UC1AddressBook book = new UC1AddressBook(); // creating object of class
+            do
+            {
+                Console.WriteLine("Welcome to Address Book");
+                Console.WriteLine("1.AddNewContact\n2.ShowContact\n3.EditContact\n4.RmoveContact");
+                Console.WriteLine("\nEnter your choice");
+                int ch = Convert.ToInt32(Console.ReadLine());
 
-            book.GetContactDetails();   //through object accessing method 
-            book.putDetails();
+
+                switch (ch)
+                {
+
+                    case 1:
+                        Console.WriteLine("\nhow many contact you want to add");
+                        int n = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 0; i < n; i++)
+                        {
+                            book.GetContactDetails();
+                            Console.WriteLine("\n");    //through object accessing method 
+
+                        }
+                        break;
+                    case 2:
+                        book.putDetails();
+                        break;
+
+                    case 3:
+                        /// Edit Contact
+                        break;
+
+                    case 4:
+                        /// Remove Contact
+                        break;
+
+                    default:
+                        break;
+                }
+                Console.WriteLine("do you want to continue? press...y/n");
+                y = Console.ReadLine();
+
+            } while (yes == y);
+
+            Console.ReadLine();
+
         }
     }
 }
